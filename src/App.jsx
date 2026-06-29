81ec14570d6815af41a077806946d5846ed250a2
Chizurum <okereolujiechizurum@gmail.com>
2026-06-29 17:54:14 +0100

    Added the Art for our website

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { ScrollTrigger, SplitText } from "gsap/all"
 import gsap from "gsap"
 import Navbar from "@/src/components/Navbar"
 import Hero from "@/src/components/Hero"
+import Cocktails from "@/src/components/Cocktails"
+import About from "@/src/components/About"
+import Art from "@/src/components/Art"
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
@@ -10,6 +13,9 @@ const App = () => {
     <main>
         <Navbar />
         <Hero />
+        <Cocktails />
+        <About />
+        <Art />
     </main>
   )
 }
